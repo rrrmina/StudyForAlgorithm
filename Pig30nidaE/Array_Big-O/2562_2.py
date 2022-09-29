f5b037ba1a8456b9a4faa404684fee90553f1bfd
Pig30nidaE <Pig30nidaE@gmail.com>
2022-09-30 07:40:39 +0900

    deleted

--- Pig30nidaE/Array_Big-O/2562_2.py
+++
@@ -1,8 +0,0 @@
-def main():
-    num_list = []
-    for i in range(9):
-        num_list.append(int(input()))
-    print(f"{max(num_list)} {num_list.index(max(num_list)) + 1}")
-
-if __name__ == '__main__':
-    main()
