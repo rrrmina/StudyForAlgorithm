813f50a4ab67bd7205a75272dd1d647066a7557b
Pig30nidaE <Pig30nidaE@gmail.com>
2022-10-15 15:10:39 +0900

    chore: 1764.py add annotation / copy README.md

--- SH/2week/code/1764/1764.py
+++ SH/2week/code/1764/1764.py
@@ -7,12 +7,12 @@ def main():
     whhvtSeen = set()
 
     for _ in range(N):
-        whoDidnTHear.add(r_line().strip())
+        whoDidnTHear.add(r_line().strip()) #듣잡 인풋, set자료형
     for _ in range(M):
-        whhvtSeen.add(r_line().strip())
-    neitherHeardSeen = list(whoDidnTHear & whhvtSeen)
+        whhvtSeen.add(r_line().strip()) #보잡 인풋, set자료형
+    neitherHeardSeen = list(whoDidnTHear & whhvtSeen) #듣잡과 보잡의 교집합 : 듣보잡
     print(len(neitherHeardSeen))
-    for i in sorted(neitherHeardSeen):
+    for i in sorted(neitherHeardSeen): #정렬 후 출력
         print(i)
 
 if __name__ == '__main__':
