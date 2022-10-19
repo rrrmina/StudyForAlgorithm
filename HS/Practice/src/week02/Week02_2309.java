23757da747e1f3de9df21713534972f8f711f809
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-19 17:29:05 +0900

    study: Week02_0329, 일곱난쟁이

---
+++ HS/Practice/src/week02/Week02_2309.java
@@ -0,0 +1,39 @@
+package week02;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Week02_2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[9];
+        int sum = 0;
+        int nA = 0;
+        int nB = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                   nA = arr[i];
+                   nB = arr[j];
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(nA == arr[i] || nB == arr[i]) continue;
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
