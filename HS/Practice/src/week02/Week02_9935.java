4a16ab4c9589fce7d51c669079b0a611e47a4efe
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-21 03:38:40 +0900

    study: Week02_9935, 문자열 폭발

---
+++ HS/Practice/src/week02/Week02_9935.java
@@ -0,0 +1,41 @@
+package week02;
+
+import java.io.*;
+import java.util.Stack;
+
+// 문자열 폭발
+public class Week02_9935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Character> stack = new Stack<>();
+        String s = br.readLine();
+        String bomb = br.readLine();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            stack.push(s.charAt(i));
+            if (stack.size() >= bomb.length() && bomb.charAt(0) == stack.peek()) {
+                boolean flag = true;
+                for (int j = 1; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - 1 - j) != bomb.charAt(j)) {
+                        flag = false;
+                    }
+                }
+                if (flag) {
+                    for (int k = 0; k < bomb.length(); k++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty()) bw.write("FRULA");
+
+        while (!stack.isEmpty()) {
+            bw.write(stack.pop()+"");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
