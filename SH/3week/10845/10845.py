77d48498731b19308c81e221a1d604a191f64816
Pig30nidaE <Pig30nidaE@gmail.com>
2022-10-23 19:21:22 +0900

    study: boj 10845 clear with python and c

---
+++ SH/3week/10845/10845.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x):
+        self.queue.append(x)
+
+    def pop(self):
+        try: print(self.queue.popleft())
+        except IndexError: print(-1)
+
+    def size(self):
+        print(len(self.queue))
+
+    def empty(self):
+        if len(self.queue) == 0: print(1)
+        else: print(0)
+
+    def front(self):
+        try: print(self.queue[0])
+        except IndexError: print(-1)
+
+    def back(self):
+        try: print(self.queue[-1])
+        except IndexError: print(-1)
+
+def main():
+    from sys import stdin
+    r_line = stdin.readline #역시나 input()으로 받으면 시간초과
+    N = int(r_line())
+    queue = Queue()
+    for _ in range(N):
+        cmd_list = r_line().split()
+        try: # 명령어가 push인 경우
+            eval(f"queue.{cmd_list[0]}({cmd_list[1]})")
+        except IndexError: # 그외 경우
+            eval(f"queue.{cmd_list[0]}()")
+
+if __name__ == '__main__':
+    main()
