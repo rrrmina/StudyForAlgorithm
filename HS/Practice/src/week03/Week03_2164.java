1ea504f9ccf383cf5141003fcc680f8e37bce83b
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-10-26 20:00:31 +0900

    study: Week03_2164, 카드2

---
+++ HS/Practice/src/week03/Week03_2164.java
@@ -0,0 +1,27 @@
+package week03;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Week03_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.remove();
+            queue.add(queue.poll());
+        }
+
+        bw.write(queue.peek()+"");
+        bw.flush();
+        bw.close();
+    }
+}
