332077aebe1871567bb8925357ae6ad9794f5e86
Pig30nidaE <Pig30nidaE@gmail.com>
2022-10-27 04:48:45 +0900

    study: boj 3078 timeout.

---
+++ SH/3week/3078/3078_test.py
@@ -0,0 +1,9 @@
+from random import randint, sample
+from string import ascii_lowercase
+
+N, K = map(int, input('input N, K : ').split())
+f = open('/Users/pig30nidae/Pig30nidaE/SFA/SH/3week/3078/random_testcase.txt', 'w')
+f.write(f'{N} {K}\n')
+for _ in range(N):
+    n = randint(1, 10)
+    f.write(f'{"".join(sample(ascii_lowercase, n))}\n')
