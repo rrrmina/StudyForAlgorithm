6da8bbea94383fa7ad26ea44b2f92a6fcb5c429b
Pig30nidaE <Pig30nidaE@gmail.com>
2022-10-28 05:25:47 +0900

    chore: delete unresolved problems

--- SH/3week/code/3078/3078_test.py
+++
@@ -1,9 +0,0 @@
-from random import randint, sample
-from string import ascii_lowercase
-
-N, K = map(int, input('input N, K : ').split())
-f = open('C:\Pig30nidaE\SFA\SH\\3week\\3078/random_testcase.txt', 'w')
-f.write(f'{N} {K}\n')
-for _ in range(N):
-    n = randint(1, 10)
-    f.write(f'{"".join(sample(ascii_lowercase, n))}\n')
