612ee0383c50d3a4c0c496a42a80fece195c8397
Pig30nidaE <Pig30nidaE@gmail.com>
2022-11-14 14:05:16 +0900

    refactor: boj 1991/9934 add comments about how to solve

--- SH/6week/code/1991/1991.py
+++ SH/6week/code/1991/1991.py
@@ -12,7 +12,6 @@ def mediumTour(root: str, tree: dict, stack: list):
     if tree[root][1] != '.':
         mediumTour(tree[root][1], tree, stack)
 
-
 def potentialTour(root: str, tree: dict, stack: list):
     stack.append(root)
     if tree[root][0] != '.':
@@ -27,14 +26,14 @@ def main():
     for _ in range(n):
         root, leftChild, rightChild = map(str, input().split())
         tree[root] = [leftChild, rightChild]
-    
-    potentialTour('A', tree, stack)
+    # 결국 root부터 시작해서 전체를 순회하는 것은 똑같지만, 언제 답을 출력할지만 결정하면 된다.    
+    potentialTour('A', tree, stack) # 전위 순회는 맨 마지막에 추가
     print(''.join(stack))
     stack.clear()
-    mediumTour('A', tree, stack)
+    mediumTour('A', tree, stack) # 중위 순회는 중간에 추가
     print(''.join(stack))
     stack.clear()
-    backTour('A', tree, stack)
+    backTour('A', tree, stack) # 후위 순회는 맨 처음에 추가
     print(''.join(stack))
     stack.clear()
 
--- SH/6week/code/9934/9934.py
+++ SH/6week/code/9934/9934.py
@@ -1,24 +1,25 @@
 def makeTree(entry_order, to_escape, depth_dict: dict, depth: int):
     center_index = int(len(entry_order) / 2)
-    if center_index == 0 or center_index == to_escape:
+    if center_index == 0 or center_index == to_escape: # ceneter index가 양쪽 끝일 경우 끝까지 순회한 것임.
         return None
     left_center_index = int(len(entry_order[0:center_index]) / 2)
     right_center_index = int(((center_index + 1) + len(entry_order) - 1) / 2)
     try:
-        depth_dict[depth].extend([entry_order[left_center_index], entry_order[right_center_index]])
+        depth_dict[depth].extend([entry_order[left_center_index], entry_order[right_center_index]]) # 현재 depth에 노드들 추가.
     except KeyError:
         depth_dict[depth] = [entry_order[left_center_index], entry_order[right_center_index]]
-    makeTree(entry_order[0:center_index], to_escape, depth_dict, depth + 1)
+    # depth를 증가시켜 재귀호출.
+    makeTree(entry_order[0:center_index], to_escape, depth_dict, depth + 1) 
     makeTree(entry_order[center_index + 1:], to_escape, depth_dict, depth + 1)
 
 def main():
     k = int(input()); del k
     entry_order = list(map(int, input().split()))
     depth_dict = dict()
-    depth_dict[0] = [entry_order[int(len(entry_order) / 2)]]
+    depth_dict[0] = [entry_order[int(len(entry_order) / 2)]] # root노드 추가
     makeTree(entry_order, len(entry_order) - 1, depth_dict, 1)
-    for i in depth_dict.keys(): 
-        for j in depth_dict[i]:
+    for depth in depth_dict.keys(): 
+        for j in depth_dict[depth]:
             print(j, end=" ")
         print()
 
