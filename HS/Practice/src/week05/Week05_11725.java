efdc081902be97945d929f787270ffd9c8fb2bd7
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-18 05:11:27 +0900

    study: Week05_11725 복습

--- HS/Practice/src/week05/Week05_11725.java
+++ HS/Practice/src/week05/Week05_11725.java
@@ -5,20 +5,21 @@ import java.util.*;
 
 // 트리의 부모 찾기, 11725
 public class Week05_11725 {
-    static List<List<Integer>> input;
+    static List<List<Integer>> tree;
+    static boolean[] visited;
     static int n;
-    static int[] parents;
+    static int[] result;
 
     public static void main(String[] args) throws IOException {
-        makeInput();
+        input();
         bfs();
-        makeOutput();
+        output();
     }
 
-    private static void makeOutput() throws IOException {
+    private static void output() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 2; i < parents.length; i++) {
-            bw.write(parents[i] + "\n");
+        for (int i = 2; i < result.length; i++) {
+            bw.write(result[i] + "\n");
         }
         bw.flush();
         bw.close();
@@ -26,34 +27,38 @@ public class Week05_11725 {
 
     private static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
+        visited[1] = true;
         queue.add(1);
-        parents[1] = 1;
         while (!queue.isEmpty()) {
             int parent = queue.poll();
-            for (int i : input.get(parent)) {
-                if (parents[i] == 0) {
-                    parents[i] = parent;
+            for (int i : tree.get(parent)) {
+                if (!visited[i]) {
+                    visited[i] = true;
                     queue.add(i);
+                    result[i] = parent;
                 }
             }
         }
     }
 
-    private static void makeInput() throws IOException {
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        input = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
-            input.add(new ArrayList<>());
+
+        tree = new ArrayList<>();
+        visited = new boolean[n + 1];
+        result = new int[n + 1];
+
+        for (int i = 0; i <= n + 1; i++) {
+            tree.add(new ArrayList<>());
         }
 
         for (int i = 1; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            input.get(a).add(b);
-            input.get(b).add(a);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            tree.get(u).add(v);
+            tree.get(v).add(u);
         }
-        parents = new int[n+1];
     }
 }
