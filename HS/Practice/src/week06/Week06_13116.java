2a2d131e44ef753ef9e21387c966e7818f1d28bc
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-27 00:58:09 +0900

    chore: Week06_1991, Week06_13116 수정

--- HS/Practice/src/week06/Week06_13116.java
+++ HS/Practice/src/week06/Week06_13116.java
@@ -1,2 +1,29 @@
-package week06;public class Week06_13116 {
+package week06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Week06_13116 {
+    static int a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            int min = Math.min(a, b);
+            if (min == a) {
+                while (true) {
+
+                }
+            }
+        }
+    }
+
+
 }
