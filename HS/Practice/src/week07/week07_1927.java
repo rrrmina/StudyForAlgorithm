2bb1d97c0f46e1d35456e5efa7766414ebd6c79a
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-27 01:39:49 +0900

    study: week07_1927, 최소힙

---
+++ HS/Practice/src/week07/week07_1927.java
@@ -0,0 +1,28 @@
+package week07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// 최소힙
+public class week07_1927 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(x -> x));
+
+        for(int i = 0 ; i < n; i++){
+            int val = Integer.parseInt(br.readLine());
+            if(val == 0){
+                if(queue.isEmpty()) System.out.println("0");
+                else System.out.println(queue.poll());
+            }else{
+                queue.add(val);
+            }
+        }
+    }
+}
