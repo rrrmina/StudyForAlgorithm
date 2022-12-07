b8bdfc8d1b34fc38701dcf5536a6896f329b1ec4
Pig30nidaE <Pig30nidaE@gmail.com>
2022-12-07 19:26:57 +0900

    refactor: boj 1890 add comment about how to solve

--- SH/9week/code/1890/1890.py
+++ SH/9week/code/1890/1890.py
@@ -4,16 +4,16 @@ setrecursionlimit(10**9)
 memo = dict()
 def getRoute(square: list, row, col, n)->int:
     global memo
-    if row >= n or col >= n or square[row][col] == 0:
-        if row == n - 1 and col == n - 1:
-            return 1
-        return 0
-    if (row, col) in memo.keys():
-        return memo[(row, col)]
-    else:
+    if row >= n or col >= n or square[row][col] == 0: # return 조건
+        if row == n - 1 and col == n - 1: # 만약 목적지 도달시
+            return 1 # count 1 return
+        return 0 # 아닌 경우는 조건 충족X
+    if (row, col) in memo.keys(): # 이미 거쳐간 경로였다면
+        return memo[(row, col)] # 메모해둔 값 return
+    else: # 재귀를 돌릴건데, 돌리면서 나온 값 메모해두기
         temp = getRoute(square, row, col + square[row][col], n) + getRoute(square, row + square[row][col], col, n)
         memo[(row, col)] = temp
-        return temp
+        return temp # 해당 값 리턴
 
 def main():
     input = stdin.readline
