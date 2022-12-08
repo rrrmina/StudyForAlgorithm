d8e2de8b445312165ff4e8ad88f4abbde2d5be8c
Pig30nidaE <Pig30nidaE@gmail.com>
2022-12-09 05:24:24 +0900

    study: boj 1965 clear, but need to study about LIS

--- SH/9week/code/1965/1965.py
+++ SH/9week/code/1965/1965.py
@@ -1,17 +1,8 @@
-def main():
-    n = int(input())
-    index_dict = dict()
-    temp = list(map(int, input().split()))
-    for i in range(n):
-        try:
-            index_dict[temp[i]].append(i)
-        except KeyError:
-            index_dict[temp[i]] = [i]
-    sorted_temp = sorted(temp)
-    sorted_index = dict()
-    for i in range(n):
-        sorted_index[i] = sorted_temp
-        
-
-if __name__ == '__main__':
-    main()
+n = int(input())
+num_list = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+for i in range(1,n+1):
+    for j in range(i+1):
+        if num_list[j] < num_list[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
